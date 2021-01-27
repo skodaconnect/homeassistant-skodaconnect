fb544745b95d8921d93f6265c5710fb7d12de6e6
Farfar <faekie@hotmail.com>
2021-01-27 13:08:13 +0100

    Added requirement for library 1.0.30rc 	ändrad:        custom_components/skodaconnect/__init__.py 	ändrad:        custom_components/skodaconnect/manifest.json

--- custom_components/skodaconnect/__init__.py
+++ custom_components/skodaconnect/__init__.py
@@ -26,7 +26,7 @@ from skodaconnect import Connection
 
 # from . import skoda
 
-__version__ = "1.0.30-rc"
+__version__ = "1.0.30-rc2"
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "skodaconnect"
