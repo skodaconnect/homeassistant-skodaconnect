b5fa977b0fd5d7c5e3c80574808454b6e794a035
Farfar <faekie@hotmail.com>
2021-01-28 15:10:35 +0100

    Fixes for pre heater 	ändrad:        README.md 	ändrad:        custom_components/skodaconnect/__init__.py 	ändrad:        custom_components/skodaconnect/manifest.json 	ändrad:        requirements.txt

--- custom_components/skodaconnect/__init__.py
+++ custom_components/skodaconnect/__init__.py
@@ -26,7 +26,7 @@ from skodaconnect import Connection
 
 # from . import skoda
 
-__version__ = "1.0.30-rc4"
+__version__ = "1.0.30-rc5"
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "skodaconnect"
