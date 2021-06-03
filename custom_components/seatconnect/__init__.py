a45352d26fb2fac7fd8403b058608d4f478b1de7
Kim <faekie@hotmail.com>
2021-06-03 11:52:49 +0200

    Update __init__.py
    Version bump

--- custom_components/seatconnect/__init__.py
+++ custom_components/seatconnect/__init__.py
@@ -24,7 +24,7 @@ from homeassistant.helpers.icon import icon_for_battery_level
 from homeassistant.util.dt import utcnow
 from seatconnect import Connection
 
-__version__ = "1.0.31"
+__version__ = "1.0.32"
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "seatconnect"
