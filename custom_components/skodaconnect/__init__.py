b520c9db16c1e4e07f55c905630fe41c95d71df4
Farfar <faekie@hotmail.com>
2021-06-04 16:11:11 +0200

    ändrad:        custom_components/skodaconnect/__init__.py

--- custom_components/skodaconnect/__init__.py
+++ custom_components/skodaconnect/__init__.py
@@ -98,7 +98,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     }
 
     # Register entity service
-    platform = entity_platform.current_platform.get()
+    #platform = entity_platform.current_platform.get()
+    platform = entity_platform.async_get_current_platform()
 
     # Register entity services
     platform.async_register_entity_service(
